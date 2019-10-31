fc7fab82ac1f3dcd635554a23016322e0c86c48c
itai <itai.shulhami@gmail.com>
2019-10-31 15:28:28 +0200

    init commit

--- SMTPClientTest (1).py
+++ SMTPClientTest (1).py
@@ -1,6 +1,7 @@
 import smtplib
 import email.utils
 import socket
+import imaplib
 from email.mime.text import MIMEText
 
 
@@ -24,10 +25,22 @@ if(realpass==password):
     server.set_debuglevel(True)  # show communication with the server
     try:
         server.sendmail(IPAddr, '172.16.10.159', msg.as_string())
-        print "sending email..."
     finally:
+        print "sending email..."
         server.quit()
 else:
     print "this isn't the password, you cant enter mine email server, :(("
+
+'''
+M = imaplib.IMAP4('172.16.10.159',143)
+M.login(getpass.getuser(), getpass.getpass())
+M.select()
+typ, data = M.search(None, 'ALL')
+for num in data[0].split():
+    typ, data = M.fetch(num, '(RFC822)')
+    print 'Message %s\n%s\n' % (num, data[0][1])
+M.close()
+M.logout()
+'''
 
 
