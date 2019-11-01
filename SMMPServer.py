b9e259070787f61e1bf736dcadfcca795ebbd107
itai <itai.shulhami@gmail.com>
2019-11-01 09:42:07 +0200

    updating

--- SMMPServer.py
+++ SMMPServer.py
@@ -14,6 +14,7 @@ print("Your Computer IP Address is: " + IPAddr)
 class CustomSMTPServer(SMTPServer):
   def process_message(self, peer, mailfrom, rcpttos, data):
     print('Receiving message from:', peer)
+    print("----------------------")
     print('Message addressed from:', mailfrom)
     print('Message addressed to:', rcpttos)
     print('Message length:', len(data))
@@ -26,14 +27,6 @@ class CustomSMTPServer(SMTPServer):
         if(iplists[i]==peer[0]):
             #saving all the  messages
             rs.set(iplists[i],data+"-end), ")
-            server = smtplib.SMTP(mailfrom, 1025)
-            print "opening connection to smtp server"
-            server.set_debuglevel(True)  # show communication with the server
-            try:
-                server.sendmail(IPAddr, '172.16.10.98', data['body'])
-                print "sending email..."
-            finally:
-                server.quit()
                 '''
     return
 #  def login(self,user,password):
@@ -49,7 +42,7 @@ if __name__ == '__main__':
     x= rs.get('10.100.102.6')
     print x+"redis working"
 #    imapObj=imaplib.IMAP4()
-    server = CustomSMTPServer(('172.16.10.159', 1025), None)
+    server = CustomSMTPServer((IPAddr, 1025), None)
     print "going to loop and run the server"
     asyncore.loop()
-    rs.close()
+   # rs.close()
--- SMTPClientTest (1).py
+++ SMTPClientTest (1).py
@@ -10,8 +10,8 @@ IPAddr = socket.gethostbyname( socket.gethostname())
 print("Your Computer IP Address is: " + IPAddr)
 # Create the message
 msg = MIMEText('This is the body of the message.')
-msg['To'] = email.utils.formataddr(('Recipient-', IPAddr))
-msg['From'] = email.utils.formataddr(('Author-', '172.16.10.98'))
+msg['From'] = email.utils.formataddr(('Recipient-', IPAddr))
+msg['To'] = email.utils.formataddr(('Author-', '172.16.10.186'))
 msg['Subject'] = 'Simple test message'
 
 
@@ -20,27 +20,17 @@ password=raw_input("enter password to connect: ")
 
 if(realpass==password):
     # remember to change the ip of the server
-    server = smtplib.SMTP('172.16.10.159', 1025)
+    server = smtplib.SMTP(IPAddr, 1025)
     print "opening connection to smtp server"
     server.set_debuglevel(True)  # show communication with the server
     try:
-        server.sendmail(IPAddr, '172.16.10.159', msg.as_string())
+        server.sendmail(IPAddr, '172.16.10.186', msg.as_string())
     finally:
         print "sending email..."
         server.quit()
 else:
     print "this isn't the password, you cant enter mine email server, :(("
 
-'''
-M = imaplib.IMAP4('172.16.10.159',143)
-M.login(getpass.getuser(), getpass.getpass())
-M.select()
-typ, data = M.search(None, 'ALL')
-for num in data[0].split():
-    typ, data = M.fetch(num, '(RFC822)')
-    print 'Message %s\n%s\n' % (num, data[0][1])
-M.close()
-M.logout()
-'''
+
 
 
