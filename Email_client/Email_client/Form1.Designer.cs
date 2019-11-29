04e517cbbd08f6dbaca746cbf6321248b8840b66
u101040 <u101040@deshalit.local>
2019-11-29 10:01:05 +0200

    winforms start

---
+++ Email_client/Email_client/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Email_client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.IpBox = new System.Windows.Forms.TextBox();
+            this.SubjectBox = new System.Windows.Forms.TextBox();
+            this.TextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MessagesBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter ip: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter subject: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(12, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter text: ";
+            // 
+            // IpBox
+            // 
+            this.IpBox.Location = new System.Drawing.Point(91, 30);
+            this.IpBox.Name = "IpBox";
+            this.IpBox.Size = new System.Drawing.Size(217, 20);
+            this.IpBox.TabIndex = 3;
+            // 
+            // SubjectBox
+            // 
+            this.SubjectBox.Location = new System.Drawing.Point(130, 62);
+            this.SubjectBox.Name = "SubjectBox";
+            this.SubjectBox.Size = new System.Drawing.Size(178, 20);
+            this.SubjectBox.TabIndex = 4;
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(104, 101);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(275, 128);
+            this.TextBox.TabIndex = 6;
+            this.TextBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.Location = new System.Drawing.Point(16, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 68);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Send Email";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.Location = new System.Drawing.Point(3, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "messages:";
+            // 
+            // MessagesBox
+            // 
+            this.MessagesBox.Location = new System.Drawing.Point(16, 274);
+            this.MessagesBox.Name = "MessagesBox";
+            this.MessagesBox.Size = new System.Drawing.Size(385, 237);
+            this.MessagesBox.TabIndex = 9;
+            this.MessagesBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 535);
+            this.Controls.Add(this.MessagesBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.SubjectBox);
+            this.Controls.Add(this.IpBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Client  Window";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox IpBox;
+        private System.Windows.Forms.TextBox SubjectBox;
+        private System.Windows.Forms.RichTextBox TextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox MessagesBox;
+    }
+}
+
---
+++ Email_client/Email_client/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Email_client
+{
+    public partial class Form1 : Form
+    {
+        private Socket send;
+        private IPEndPoint remoteEP;
+        private byte[] bytes;
+        public Form1()
+        {
+            InitializeComponent();
+            byte[] bytes = new byte[1024];
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+            Socket send = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream, ProtocolType.Tcp);
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            send.Connect(remoteEP);
+            Console.WriteLine("connected");
+            string ip = this.IpBox.Text;
+            string subject = this.SubjectBox.Text;
+            string text = this.TextBox.Text;
+            this.IpBox.Text = null;
+            this.SubjectBox.Text = null;
+            this.TextBox.Text = null;
+            Console.WriteLine("send ip");
+            byte[] msg = Encoding.ASCII.GetBytes(ip);
+            int bytesSent = send.Send(msg);
+            int bytesRec = send.Receive(bytes);
+            Console.WriteLine("Echoed test = {0}",
+            Encoding.ASCII.GetString(bytes, 0, bytesRec)); 
+            Console.WriteLine("send subject");
+            byte[] msg2 = Encoding.ASCII.GetBytes(subject);
+            int bytesSent2 = send.Send(msg2);
+            int bytesRec2 = send.Receive(bytes);
+            Console.WriteLine("Echoed test = {0}",
+            Encoding.ASCII.GetString(bytes, 0, bytesRec2));
+            Console.WriteLine("send text");
+            byte[] msg3 = Encoding.ASCII.GetBytes(text);
+            int bytesSent3 = send.Send(msg3);
+            int bytesRec3 = send.Receive(bytes);
+            Console.WriteLine("Echoed test = {0}",
+            Encoding.ASCII.GetString(bytes, 0, bytesRec3));
+
+        }
+    }
+    
+}
