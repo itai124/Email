d3e597e98f2665590c2b2b8408232ab5f5971e39
itai124 <55763165+itai124@users.noreply.github.com>
2020-01-02 15:57:15 +0200

    Add files via upload

---
+++ Itai.py
@@ -0,0 +1,56 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+
+def generate_a_keys():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend())
+    return private_key
+
+def generate_key():
+    key = Fernet.generate_key()
+    return key
+
+def encrypt_a_msg(message, public_key):
+    encrypted = public_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return encrypted
+
+def decrypt_a_msg(encrypted, private_key):
+    original = private_key.decrypt(
+        encrypted,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return original
+
+msg = "I want email"
+private_key = generate_a_keys()#generates public and private keys
+public_key = private_key.public_key()
+encrypted_msg = encrypt_a_msg(msg, public_key)
+print encrypted_msg
+decrypted_msg = decrypt_a_msg(encrypted_msg, private_key)
+print "------------------------------\n"+decrypted_msg
+
+
+
+
+
+
+
