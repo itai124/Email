6971f67189a5ae705e187f0e9e83dd87ab504d16
dotan <dooda174@gmail.com>
2020-02-11 15:11:09 +0200

    need to fix database prob

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,5 @@
+#for flask
 import os
-#import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from _init_ import app, db, bcrypt
@@ -9,8 +9,121 @@ from _init_ import app, db, bcrypt
 from forms import RegistrationForm, LoginForm
 from models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+#for sockets with server
+import pprint
+import imaplib
+import mailbox
+import email.utils
+import threading
+import localmail
+import smtplib
+import socket
+from socket import *
+import redis
+import  pickle
+import ssl
+from email.mime.text import MIMEText
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+#
 
 
+#defs--------------------------------------------------------
+def generate_a_keys():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048*3,
+        backend=default_backend())
+    return private_key
+
+def generate_key():
+    key = Fernet.generate_key()
+    return key
+
+def encrypt_a_msg(message, public_key):
+    encrypted = public_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return encrypted
+
+def decrypt_a_msg(encrypted, private_key):
+    original = private_key.decrypt(
+        encrypted,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return original
+
+def connect_send(src,dst,subject,txt):
+    TCPclientsock = socket(AF_INET,SOCK_STREAM)
+    #TCPclientsock.settimeout(10)
+    TCPclientsock.connect((Host, Port))
+    print "allowed"
+    private_key = generate_a_keys()  # generates public and private keys
+    public_key = private_key.public_key()
+    serialized_public_key=public_key.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.PKCS1)#serialize the key
+    serialized_public_key=pickle.dumps(serialized_public_key)# pickle the key
+    TCPclientsock.send(serialized_public_key)# send the key
+    server_public_key = TCPclientsock.recv(buffsize)#recv key from server
+    server_public_key=pickle.loads(server_public_key)#load the key
+    server_public_key= load_pem_public_key(server_public_key, backend=default_backend())#back to the first form
+    data=["S",dst,subject,txt]
+    byted_data= pickle.dumps(data)
+    encrypted_msg = encrypt_a_msg(byted_data, server_public_key)
+    print "encrypted "+encrypted_msg
+    TCPclientsock.send(encrypted_msg)
+    server_data = TCPclientsock.recv(buffsize)
+    decrypted_msg = decrypt_a_msg(server_data,private_key)
+    print "after decrypte "+ decrypted_msg
+    
+def connect_add(dst,subject,txt):
+    TCPclientsock = socket(AF_INET,SOCK_STREAM)
+    #TCPclientsock.settimeout(10)
+    TCPclientsock.connect((Host, Port))
+    print "allowed"
+    private_key = generate_a_keys()  # generates public and private keys
+    public_key = private_key.public_key()
+    serialized_public_key=public_key.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.PKCS1)#serialize the key
+    serialized_public_key=pickle.dumps(serialized_public_key)# pickle the key
+    TCPclientsock.send(serialized_public_key)# send the key
+    server_public_key = TCPclientsock.recv(buffsize)#recv key from server
+    server_public_key=pickle.loads(server_public_key)#load the key
+    server_public_key= load_pem_public_key(server_public_key, backend=default_backend())#back to the first form
+    print "T"
+    data=["F",None,None,None]
+    byted_data = pickle.dumps(data)
+    encrypted_msg = encrypt_a_msg(byted_data, server_public_key)
+    print "encrypted "+encrypted_msg
+    TCPclientsock.send(encrypted_msg)
+    server_data = TCPclientsock.recv(buffsize)
+    decrypted_msg = decrypt_a_msg(server_data,private_key)
+    print "after decrypte "+decrypted_msg
+    got_emails=pickle.loads(decrypted_msg)
+    for email in got_emails:
+        pprint.pprint(email)
+
+
+
+
+
+Host = '172.16.10.186'
+Port = 50003
+buffsize = 1024*9
+Addr = (Host,Port)
 
 
 db.create_all()
@@ -107,9 +220,10 @@ def new_post():
     form = PostForm()
     if form.validate_on_submit():
         print form.to.data
-        post = Post(title=form.title.data, content=form.content.data, to=form.to.data, author=current_user,)
+        post = Post(title=form.title.data, content=form.content.data, to=form.to.data, author=current_user)
         db.session.add(post)
         db.session.commit()
+        #connect_send(current_user.username,form.to.data,form.title.data,form.content.data)
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Email',
@@ -159,10 +273,4 @@ def delete_post(post_id):
     flash('Your Email has been deleted!', 'success')
     return redirect(url_for('home'))
 
-'''
-@app.route("/account")
-@login_required
-def account():
-    image_file = url_for('static',filename='profile_pics/' + current_user.image_file)
-        return render_template('account.html', title='Account', image_file=image_file)
-'''
+
